7addd7d11919cf829d9ef9c9d030280c340941b7
Redfish <lesonduko@gmail.com>
2016-07-26 16:09:06 +0200

    avancer de la resolution du pomodoro en jquery (bouton stop et defilement temps)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,87 @@
-/* * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * *
- * * YOUR CODE GOES HERE ! * *
- * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * */
+var temps = 1;
+var minuter_min = temps; 
+var minuter_sec;
+var set;
+/*var nb_sec_in_min = 60;*/
+
+
+/*defined the name task*/
+function setName(){
+	var taskName = $("#task").val();
+	/* throw an error if the task field is empty */
+	if (taskName == ""){ alert("Erreur : rien n'est definit "); return false; }
+	$("h2:first").text("Doing " + taskName);
+	$("#task").val("");
+}
+
+
+$("#play").click(function() {
+	setName();
+	startTimer();
+
+});
+
+/* algo in js todo convert in jquery*/
+/*timer*/
+
+function timer(){
+ $("p:first").text( temps +" : "+" 00");
+ 
+};
+
+function startTimer(){
+	minuter_min = temps;
+	minuter_sec = 00;
+	set = setInterval(DeroulTimer,1000);
+}
+
+function afficheTime(){
+	var t = minuter_min + ":" + minuter_sec;
+	if(minuter_sec < 10)
+		t = minuter_min + ":0" + minuter_sec;
+	$("p:first").text(t);
+}
+function DeroulTimer(){
+	afficheTime();
+	if(minuter_sec == 00 && minuter_min != 00){
+		minuter_sec = 59;
+		minuter_min -= 1;   
+	}
+	else{
+		minuter_sec = minuter_sec -1;
+	}
+}
+
+$("#stop").click(function() {
+	StopTimer();
+});
+	
+	function StopTimer(){
+		var temps = "00" + ":" + "00"; 
+		$("p:first").text(temps);
+		clearInterval(set);
+		$("#task").text(task);
+		$("#done").val("");
+				
+
+
+
+	};
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
